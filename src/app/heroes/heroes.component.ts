fa1ea19f79b0173ba93f0790ce8e01484bafd672
Edward <edward.davenport97@gmx.de>
2022-01-25 14:11:14 +0100

    First Commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
+import {HeroService} from "../hero-service/hero.service";
+import {MessageService} from "../messages-service/message.service";
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, public messageService: MessageService) {
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+}
